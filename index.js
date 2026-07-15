443f4ca8052b9938a074348e718ff7953f03dba4
youssefrabie430-arch <youssefrabie430@gmail.com>
2026-07-15 07:21:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const noblox = require('noblox.js');
 const axios = require('axios');
 const { createCanvas, loadImage } = require('canvas');
 const pricePerRobux = 0.27; // ده سعر الروبكس الواحد بالجنيه
-const cryptoRate = 0.005; 
+const cryptoRate = 0.0061;
 
 const client = new Client({
     intents: [
